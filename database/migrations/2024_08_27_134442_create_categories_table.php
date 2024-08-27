695cf41ecb9d3477c7ca0eb84bde13121a0e45ea
Elias Galvão Alves <galvaoalveselias@gmail.com>
2024-08-27 10:54:03 -0300

    first commit

---
+++ database/migrations/2024_08_27_134442_create_categories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable(false);
+            $table->string('color')->default('#808080');
+            $table->foreignIdFor(User::class)->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->dropForeignIdFor(User::class);
+        });
+        Schema::dropIfExists('categories');
+    }
+};

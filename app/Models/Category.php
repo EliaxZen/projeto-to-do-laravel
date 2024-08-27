fde9b06b9e409c5393cb85655f9cffe1a168e7d7
Elias Galvão Alves <galvaoalveselias@gmail.com>
2024-08-27 18:12:27 -0300

    Configurando os Models e relacionamentos a nível de código

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,10 +2,26 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Task;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Category extends Model
 {
     use HasFactory;
+
+    protected $fillabe = [
+        'title',
+        'color',
+        'user_id',
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function tasks(){
+        return $this->hasMany(Task::class);
+    }
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,10 +2,28 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Task extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'due_date',
+        'category_id',
+        'user_id',
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
 }

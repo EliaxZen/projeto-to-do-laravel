2d146221f1db712c8897a0544d6c3902755bc167
Elias Galvão Alves <galvaoalveselias@gmail.com>
2024-08-28 17:59:10 -0300

    criando template, configuração inicial

---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TodoApp</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900 display=swap"rel="stylesheet">
+    <link rel="stylesheet" href="/assets/css/styles.css" />
+</head>
+
+<body>
+    a
+</body>
+
+</html>

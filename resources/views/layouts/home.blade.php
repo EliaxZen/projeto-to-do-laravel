9a007a94b35e4c1ce8c1a75def077fab0e6cf57f
Elias Galvão Alves <galvaoalveselias@gmail.com>
2024-08-29 17:51:25 -0300

    Task List - Estrutura Básica do Template

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -45,6 +45,21 @@
                             <option value="1">Todas as tarefas</option>
                         </select>
                     </div>
+                    <div class="task_list">
+                        <div class="task">
+                            <div class="title">
+                                <input type="checkbox">
+                                <div class="task_title">Titulo da Tarefa</div>
+                            </div>
+                            <div class="priority">
+                                <div class="sphere"></div>
+                                <div>prioridade</div>
+                            </div>
+                            <div class="actions">
+                                Editar - Excluir
+                            </div>
+                        </div>
+                    </div>
                 </section>
             </main>
         </div>

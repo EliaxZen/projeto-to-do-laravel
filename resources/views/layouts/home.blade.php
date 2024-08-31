4d0dd52b12b44919b606dd3d2d2018125861bc88
Elias Galvão Alves <galvaoalveselias@gmail.com>
2024-08-31 11:43:50 -0300

    ajustes finais da página principal

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -29,7 +29,12 @@
                     <div class="graph_header">
                         <h2>Progresso do Dia</h2>
                         <hr class="linha_header" />
-                        <div class="graph_header_date">Data</div>
+                        <div class="graph_header_date">
+                            <img src="/assets/images/icon-prev-orange.png" alt="">
+                                12 de outubro
+                            <img src="/assets/images/icon-next-orange.png" alt="">
+
+                        </div>
                     </div>
                     <div class="graph_header_subtitle">Tarefas: <b>3/6</b></div>
                     <div class="graph_box">
@@ -37,7 +42,7 @@
                         <div class="graph_placeholder"></div>
 
                         <div class="tasks_left_footer">
-                            <img src="/assets/images/icon-info-orange.png" alt=""/>
+                            <img src="/assets/images/icon-info-orange.png" alt="" />
                             Restam 3 tarefas para serem realizadas
                         </div>
 
@@ -45,9 +50,11 @@
                 </section>
                 <section class="list">
                     <div class="list_header">
-                        <select class="list_header_select" name="" id="">
-                            <option value="1">Todas as tarefas</option>
-                        </select>
+                        <div class="list_header_select-container">
+                            <select class="list_header_select" name="" id="">
+                                <option value="1">Todas as tarefas</option>
+                            </select>
+                        </div>
                     </div>
                     <div class="task_list">
                         <div class="task">

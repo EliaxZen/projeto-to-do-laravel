cd3044795c803be9bf44789deadf1327f20c152e
Elias Galvão Alves <galvaoalveselias@gmail.com>
2024-09-01 10:10:44 -0300

    criando tela de inclusão de tasks

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,5 +5,27 @@
         </a>
     </x-slot:btn>
 
-    Tela de Criar uma Tarefa
+    <section id="create_task_section">
+        <h1>Criar Tarefa</h1>
+        <form action="">
+            <div class="input_area">
+                <label for="title">Titulo da Task</label>
+                <input name="title" id="title" required />
+            </div>
+            <div class="input_area">
+                <label for="due_date">Data de Realização</label>
+                <input type="date" id="due_date" name="due_date" required />
+            </div>
+            <div class="input_area">
+                <label for="category">Categoria</label>
+                <select name="category" required id="category">
+                    <option selected disabled value="">Selecione a categoria</option>
+                </select>
+            </div>
+            <div class="input_area">
+                <label for="title">Descrição da tarefa</label>
+                <textarea name="description" placeholder="Digite uma descrição para sua tarefa"></textarea>
+            </div>
+        </form>
+    </section>
 </x-layout>
